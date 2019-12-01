d205eb4b517b8c25c9cccffeb3f4d5d66aacde4b
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-12-01 19:41:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,5 @@ require 'pry'
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+#.gsub("%", "").to_i
